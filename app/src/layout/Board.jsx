f4d8ffe49716406a5fb8ff90b65531b4954c93fb
Joshua <joshuarvlcb@gmail.com>
2023-03-24 22:11:33 -0700

    finished table

--- app/src/layout/Board.jsx
+++ app/src/layout/Board.jsx
@@ -1,45 +1,53 @@
-import React,{useState,useEffect,useRef} from "react";
-import Cell from "../components/Cell"
+import React, { useState, useEffect, useRef } from "react";
+import Cell from "../components/Cell";
 export const Board = () => {
   /**
-   * create cell component 
-   * 
-   * get size of board 
+   * create cell component
+   *
+   * get size of board
    *  find property that will give me that info
-   * 
+   *
    * add cells to fill up board
-   * 
-   * 
+   *
+   *
    * !! Review grid
-   * 
+   *
    */
+
   const [size, setSize] = useState({
-    height:0,
-    width:0
+    height: 0,
+    width: 0,
   });
-  let boardSize = useRef(null)
-  
+  let boardSize = useRef(null);
+
   useEffect(() => {
     setSize({
-      height : boardSize.current.clientHeight - 100,
-
-      width: boardSize.current.clientWidth - 100
-      
-    })
-    
+      height: boardSize.current.clientHeight - 100,
+      width: boardSize.current.clientWidth - 100,
+    });
   }, []);
 
-  
-  return <div onClick = {() => console.log((size.height * size.width) / 2500)} ref = {boardSize} className="board" >
-    {size.height > 0 ? new Array(Math.floor(size.height / 25)).fill(null).map((_,i) => {
-      return <div className = "row" key = {i}>
-          {new Array(Math.floor(size.width / 25)).fill(null).map((_,i) => {
-            return <Cell key = {i}/>
-          })} 
-      </div> 
-    }) : "" }
-  </div>;
-
+  return (
+    <div
+      onClick={() => console.log((size.height * size.width) / 2500)}
+      ref={boardSize}
+      className="board"
+    >
+      {size.height > 0
+        ? new Array(Math.floor(size.height / 25)).fill(null).map((_, i) => {
+            return (
+              <div className="row" key={i}>
+                {new Array(Math.floor(size.width / 25))
+                  .fill(null)
+                  .map((_, i) => {
+                    return <Cell key={i} />;
+                  })}
+              </div>
+            );
+          })
+        : ""}
+    </div>
+  );
 };
 
 export default Board;
